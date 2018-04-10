df6cb6dae6a317b21de88b53cbb67fb5fb628522
mythiaa <36728456+mythiaa@users.noreply.github.com>
2018-04-10 12:06:25 +0300

    bugfix

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     var timeLeft = 1500;
     var breakLeft = 300;
     working = false;
-    //Sets the string
+    //Sets the timer string
     var string = function(){
         var minute = Math.floor(timer/60);
         var second = Math.floor(timer%60);
@@ -18,33 +18,27 @@ $(document).ready(function(){
            return str = minute + ":" + second;
         }
     }
+    //Strings for session and break lengths
     var defaultSessionString = "Session:<br>" + Math.floor(timeLeft/60)+" mins";
-    var defaultBreakString = "Break:<br>" + Math.floor(breakLeft/60)+ " mins";    
+    var defaultBreakString = "Break:<br>" + Math.floor(breakLeft/60)+ " mins";
     $("#sessionText").html(defaultSessionString);
     $("#breakText").html(defaultBreakString);
     $("#timerText").html(Math.floor(timeLeft/60)+":0"+Math.floor(timeLeft%60));
     //Buttons
     $("#reset").attr("disabled", true);
     $("#start").click(function(){
-        if (working === false){session();
-        console.log("start");
-        $('#start').attr("disabled", true);
-        $("#reset").attr("disabled", false);
-        $("#breakIncrease").attr("disabled", true);
-        $("#breakDecrease").attr("disabled", true);
-        $("#sessionIncrease").attr("disabled", true);
-        $("#sessionDecrease").attr("disabled", true);
-    }            
+        if (working === false){
+            session();
+            $('#start').attr("disabled", true);
+            $("#reset").attr("disabled", false);
+            $("#breakIncrease").attr("disabled", true);
+            $("#breakDecrease").attr("disabled", true);
+            $("#sessionIncrease").attr("disabled", true);
+            $("#sessionDecrease").attr("disabled", true);
+        }
     });
     $("#reset").click(function(){
-        if (working === true){reset();console.log("reset");
-        $('#start').attr("disabled", false);
-        $("#breakIncrease").attr("disabled", false);
-        $("#breakDecrease").attr("disabled", false);
-        $("#sessionIncrease").attr("disabled", false);
-        $("#sessionDecrease").attr("disabled", false);
-        $("#timerText").html(Math.floor(timeLeft/60)+":0"+Math.floor(timeLeft%60));
-    }
+        if (working === true){reset();}
     })
     $("#sessionIncrease").click(function(){
         timeLeft = timeLeft+60;
@@ -55,8 +49,7 @@ $(document).ready(function(){
             timeLeft = 1800;
             $("#sessionText").html("Session:<br>30 mins");
             $("#timerText").html("30:00");
-
-        }        
+        }
     });
     $("#sessionDecrease").click(function(){
         timeLeft = timeLeft-60;
@@ -67,43 +60,37 @@ $(document).ready(function(){
             timeLeft = 60;
             $("#sessionText").html("Session:<br>1 min");
             $("#timerText").html("1:00");
-
-
-        }        
+        }
     });
     $("#breakIncrease").click(function(){
         breakLeft = breakLeft+60;
         let defaultBreakString = "Break:<br>" + Math.floor(breakLeft/60) +" mins";
-        $("#breakText").html(defaultBreakString);   
+        $("#breakText").html(defaultBreakString);
         if(breakLeft > 600){
             breakLeft = 600;
             $("#breakText").html("Break:<br>10 mins");
-        }        
+        }
     });
     $("#breakDecrease").click(function(){
         breakLeft = breakLeft-60;
         let defaultBreakString = "Break:<br>" + Math.floor(breakLeft/60) +" mins";
-        $("#breakText").html(defaultBreakString);   
+        $("#breakText").html(defaultBreakString);
         if(breakLeft < 120){
             breakLeft = 60;
             $("#breakText").html("Break:<br>1 min");
-        }        
+        }
     });
-    
-    
     //Session timer function
     function session(){
         working = true;
         timer = timeLeft;
         testObject.sessionCountdown = setInterval(function(){
-            console.log("session");
             timer--;
             $("#timerText").html(string);
-            if (timer <= 0) {  
-                console.log("session stop");
+            if (timer <= 0) {
                 clearInterval(testObject.sessionCountdown);
                 $("#timerText").html("");
-                mola();         
+                mola();
             }
         },1000)
     }
@@ -112,27 +99,31 @@ $(document).ready(function(){
         working = true;
         timer = breakLeft
         testObject.timeout = setInterval(function(){  
-            timer--;          
-            console.log(timer);  
+            timer--;
             $("#timerText").html(string);
             if(timer <= 0) {
-                console.log("mola stop");
                 clearInterval(testObject.timeout);
                 $("#timerText").html("");
                 session();
             }
         },1000)
-    }    
+    }
     //Reset function
     function reset(){
+        $('#start').attr("disabled", false);
+        $("#breakIncrease").attr("disabled", false);
+        $("#breakDecrease").attr("disabled", false);
+        $("#sessionIncrease").attr("disabled", false);
+        $("#sessionDecrease").attr("disabled", false);
         working = false;
-        let timeLeft = 1500;
-        let breakLeft = 300;
-        var defaultSessionString = "Session:<br>" + Math.floor(timeLeft/60)+" mins";
-        var defaultBreakString = "Break:<br>" + Math.floor(breakLeft/60)+ " mins";    
+        timeLeft = 1500;
+        breakLeft = 300;
+        defaultSessionString = "Session:<br>" + Math.floor(timeLeft/60)+" mins";
+        defaultBreakString = "Break:<br>" + Math.floor(breakLeft/60)+ " mins";
         clearInterval(testObject.sessionCountdown);
         clearInterval(testObject.timeout);
         $("#sessionText").html(defaultSessionString);
         $("#breakText").html(defaultBreakString);
+        $("#timerText").html(Math.floor(timeLeft/60)+":0"+Math.floor(timeLeft%60));
     }
-  })
+})
